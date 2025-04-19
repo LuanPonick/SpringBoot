cc3b2f5fd2a0cf19d0b6efd1302012d0640e3a31
luanPonick <luanponick07@gmail.com>
2025-04-19 18:36:01 -0300

    start

---
+++ estudo seguindo documentação/helloWorld/src/main/java/com/helloWorld/helloWorld/HelloController.java
@@ -0,0 +1,19 @@
+package com.helloWorld.helloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		System.out.println("********");
+		System.out.println("********");
+		System.out.println("*teste");
+		System.out.println("********");
+		System.out.println("********");
+		return "ola mundo";
+	}
+
+}
